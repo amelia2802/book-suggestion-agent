fed4f2ec6ecfb262508a0233f2ce4fc2051c1c2c
Amelia Dutta <amelia280111dutta@gmail.com>
2025-07-19 23:53:36 +0530

    Add book search functionality and update project configuration files

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
-	_ "github.com/hypermodeinc/modus/sdk/go"
+
+	"github.com/hypermodeinc/modus/sdk/go/pkg/http"
+	"github.com/hypermodeinc/modus/sdk/go/pkg/models"
+	"github.com/hypermodeinc/modus/sdk/go/pkg/models/openai"
+	
 )
 
-func SayHello(name *string) string {
+type Book struct {
+	Title string `json:"title"`
+	AuthorName []string `json:"author_name"`
+	Subject []string `json:"subject"`
+}
 
-	var s string
-	if name == nil {
-		s = "World"
-	} else {
-		s = *name
+type SearchResponse struct{
+	NumFound int `json:"numFound"`
+	Docs []Book `json:"docs"`
+}
+
+func SearchBooks(query string) (*SearchResponse, error){
+	url := fmt.Sprintf("https://openlibrary.org/search.json?q=%s&limit=5",query)
+
+	response, err := http.Fetch(url)
+
+	if err!= nil {
+		return nil, err
 	}
 
-	return fmt.Sprintf("Hello, %s!", s)
+	if !response.Ok() {
+		return nil, fmt.Errorf("search failed: %d %s", response.Status, response.StatusText)
+	}
+
+	var searchResult SearchResponse
+	err = response.JSON(&searchResult)
+	if err != nil {
+		return nil, err
+	}
+
+	return &searchResult, nil
 }
+
+func CategorizeBook(query string) (*BookCategory, error){
+	books, err := SearchBooks(query)
+	if err != nil {
+		return nil, err
+	}
+}
+
+model, err := models.GetModel[]
